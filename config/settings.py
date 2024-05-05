64f4d662274ae04fca66e7334e872e01abf98400
joelproxi <joeledmond95@gmail.com>
2024-05-05 20:09:35 +0100

    en '*' in allowed_hots

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-18!13#=#i2zv5j$79=tx6sknfd*5eqe)(ri)q9yp8g^v3=pd*=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
